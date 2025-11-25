3264574c2391e899d187c2d70c16130824b9cefd
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-11-24 20:47:02 -0800

    done exercise

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-$(function() {
-  // YOUR CODE HERE
-})
+$(function () {
+  $("#toggleButton").on("click", function () {
+    $(this).next("p").slideToggle();
+  });
+
+  $("#colorButton").on("click", function () {
+    $(this).next("div").css({
+      backgroundColor: "yellow",
+    });
+  });
+
+  $("#addClassButton").on("click", function () {
+    $("#classDiv").addClass("newClass");
+  });
+  $("#removeClassButton").on("click", function () {
+    $("#classDiv").removeClass("newClass");
+  });
+
+  $("#fadeInButton").on("click", function () {
+    $("#fadeDiv").fadeIn("slow");
+  });
+  $("#fadeOutButton").on("click", function () {
+    $("#fadeDiv").fadeOut("slow");
+  });
+
+  $("#slideUpButton").on("click", function () {
+    $("#slideDiv").slideUp("slow");
+  });
+  $("#slideDownButton").on("click", function () {
+    $("#slideDiv").slideDown("slow");
+  });
+});
